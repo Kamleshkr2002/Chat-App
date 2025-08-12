41038039fc7636d36f87597486afeb8acd132365
Kamlesh <kamlesh.kumar23112002@gmail.com>
2025-08-12 23:07:06 +0530

    frontend done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ const HomePage = () => {
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-      <div className={`background-blur-xl border-2 border-gray-200 rounded-2xl 
+      <div className={`backdrop-blur-xl border-2 border-gray-200 rounded-2xl 
       overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <Sidebar /> 
-        <ChatContainer/>
-        <RightSidebar />
+        <Sidebar setSelectedUser={setSelectedUser} selectedUser={selectedUser}/> 
+        <ChatContainer setSelectedUser={setSelectedUser} selectedUser={selectedUser}/>
+        <RightSidebar setSelectedUser={setSelectedUser} selectedUser={selectedUser}/>
       </div>
     </div>
   )
